2d82bf99fefda91e52a1d2246070055d2aad1aee
Rohit Vishwakarma <52505486+ROHIT25082000@users.noreply.github.com>
2022-05-28 09:37:01 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ function getJokes(e) {
     const numOfJokes = document.querySelector('input[type="number"]').value;
     const xhr = new XMLHttpRequest();
     let jokes;  
-    xhr.open('GET',`http://api.icndb.com/jokes/random/${numOfJokes}`,true);
+    xhr.open('GET',`https://api.icndb.com/jokes/random/${numOfJokes}`,true);
     
     xhr.onload = function() {
         if(this.status === 200) {
